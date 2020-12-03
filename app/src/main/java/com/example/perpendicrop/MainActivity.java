16196628ff4264e91ed154294a6e8a6aba4ee382
Disha <dishapant22@gmail.com>
2020-12-03 16:03:57 +0530

    Bottom navbar functionality

--- app/src/main/java/com/example/perpendicrop/MainActivity.java
+++ app/src/main/java/com/example/perpendicrop/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.perpendicrop;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.perpendicrop.ui.dashboard.DashboardFragment;
+import com.example.perpendicrop.ui.home.HomeFragment;
+import com.example.perpendicrop.ui.notifications.NotificationsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(navListener);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -25,9 +33,51 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-
-
+        /*NavigationUI.setupWithNavController(navView, navController);*/
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.navigation_small:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.navigation_medium:
+                            selectedFragment = new DashboardFragment();
+                            break;
+                        case R.id.navigation_large:
+                            selectedFragment = new NotificationsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, selectedFragment).commit();
+                    return true;
+                }
+            };
+
+    /*viewTransactionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.viewTransactionsAction);
+            }
+        });*/
+
+        /*@Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle item selection
+            switch (item.getItemId()) {
+                case R.id.new_game:
+                    newGame();
+                    return true;
+                case R.id.help:
+                    showHelp();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }*/
 }
